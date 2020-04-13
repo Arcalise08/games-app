7913e564fc71c54fdfacd0223989bf9a41016e47
Arcalise08 <61042848+Arcalise08@users.noreply.github.com>
2020-04-13 13:13:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -47,8 +47,7 @@ app.use(function(err, req, res, next) {
 
 app.use(cors({
   origin: function(origin, callback){
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+    if(allowedOrigins.indexOf(origin) === -1){
       var message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
       return callback(new Error(message ), false);
     }
